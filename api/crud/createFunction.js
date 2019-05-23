9afa35045f4d79cbcbed8899000a0a13dabca735
Andrei Tudor <andrei.tudor27@gmail.com>
2019-05-23 15:25:46 +0200

    Include the ODEUM-org-id

--- api/crud/createFunction.js
+++ api/crud/createFunction.js
@@ -10,14 +10,15 @@ router.put('/:version/f', async (req, res, next) => {
 
 	if (verifyAPIVersion(apiVersion)) {
 		if (authenticate(authToken)) {
-			let query = `INSERT INTO Functions
-				(name, js, description, \`type\`)
-				VALUES(?, ?, ?, ?)`
+			let query = `INSERT INTO Functions (name, js, description, \`type\`, customer_id)
+			SELECT ?, ?, ?, ?, c.id from Customer c 
+			where c.ODEUM_org_id = ?`
 			let name = req.body.name
 			let js = req.body.js
 			let description = req.body.description
 			let type = req.body.type
-			mysqlConn.query(query, [name,js,description,type]).then(rs => {
+			let ODEUMid = req.body.orgId
+			mysqlConn.query(query, [name,js,description,type, ODEUMid]).then(rs => {
 				if (rs.insertId !== null || rs.insertId !== undefined) { 
 					console.log(rs)
 					res.status(200).json(rs[0].insertId)
