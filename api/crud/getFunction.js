bb410ab6ae47d2e05b28eda44b62dcae82a7e2bb
Andrei Tudor <andrei.tudor27@gmail.com>
2019-05-23 15:26:14 +0200

    Remove duplicate

--- api/crud/getFunction.js
+++ api/crud/getFunction.js
@@ -31,34 +31,34 @@ router.get('/:version/f/:id', async (req, res, next) => {
 		res.send(`API/sigfox version: ${apiVersion} not supported`)
 	}
 })
-router.get('/:version/f/:id', async (req, res, next) => {
-	let apiVersion = req.params.version
-	let authToken = req.headers.auth
-	// let customerID = req.params.customerID
-	let funcID = parseInt(req.params.id, 10)
-	if (verifyAPIVersion(apiVersion)) {
-		if (authenticate(authToken)) {
-			let query = `SELECT f.* from Functions f
-			where f.id = ?`
-			await mysqlConn.query(query, [funcID]).then(rs => {
-				console.log(rs[0][0])
-				if (rs[0][0])
-				{
-					res.status(200).json(rs[0][0])
-				}
-				else {
-					res.status(404).json(false)
-				}
-			}).catch(err => {
-				if (err) { res.status(500).json(err) }
-			})
-		} else {
-			res.status(403).json('Unauthorized Access! 403')
-			console.log('Unauthorized Access!')
-		}
-	} else {
-		console.log(`API/sigfox version: ${apiVersion} not supported`)
-		res.send(`API/sigfox version: ${apiVersion} not supported`)
-	}
-})
+// router.get('/:version/f/:id', async (req, res, next) => {
+// 	let apiVersion = req.params.version
+// 	let authToken = req.headers.auth
+// 	// let customerID = req.params.customerID
+// 	let funcID = parseInt(req.params.id, 10)
+// 	if (verifyAPIVersion(apiVersion)) {
+// 		if (authenticate(authToken)) {
+// 			let query = `SELECT f.* from Functions f
+// 			where f.id = ?`
+// 			await mysqlConn.query(query, [funcID]).then(rs => {
+// 				console.log(rs[0][0])
+// 				if (rs[0][0])
+// 				{
+// 					res.status(200).json(rs[0][0])
+// 				}
+// 				else {
+// 					res.status(404).json(false)
+// 				}
+// 			}).catch(err => {
+// 				if (err) { res.status(500).json(err) }
+// 			})
+// 		} else {
+// 			res.status(403).json('Unauthorized Access! 403')
+// 			console.log('Unauthorized Access!')
+// 		}
+// 	} else {
+// 		console.log(`API/sigfox version: ${apiVersion} not supported`)
+// 		res.send(`API/sigfox version: ${apiVersion} not supported`)
+// 	}
+// })
 module.exports = router
