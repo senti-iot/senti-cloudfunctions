526b1e5def06a03e9484cf5c3c5fd16736b83438
Andrei Tudor <andrei.tudor27@gmail.com>
2019-07-03 13:31:44 +0200

    Edit Function

--- api/crud/updateFunction.js
+++ api/crud/updateFunction.js
@@ -10,13 +10,14 @@ router.post('/:version/f', async (req, res, next) => {
 
 	if (verifyAPIVersion(apiVersion)) {
 		if (authenticate(authToken)) {
-			let query = `UPDATE Functions
-			SET name=?, 
-			js=?, 
-			description=?, 
-			customer_id=?,
-			\`type\`=?
-			WHERE id=?;
+			let query = `UPDATE Functions as f
+			INNER JOIN Customer c on c.ODEUM_org_id = ?
+						SET f.name=?, 
+			-- 			f.js=?, 
+						f.description=?, 
+						f.customer_id=c.id,
+						\`type\`=?
+			WHERE f.id=?;
 			`
 			let id = req.body.id
 			let name = req.body.name
@@ -24,7 +25,7 @@ router.post('/:version/f', async (req, res, next) => {
 			let description = req.body.description
 			let type = req.body.type
 			let orgId = req.body.orgId
-			mysqlConn.query(query, [name, js, description, orgId, type, id]).then(rs => {
+			mysqlConn.query(query, [orgId, name, js, description, type, id]).then(rs => {
 				if (rs.insertId !== null || rs.insertId !== undefined) {
 					res.status(200).json(rs[0].info)
 				}
